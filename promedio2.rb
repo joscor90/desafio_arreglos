b0f27507a2478081c740056ea2778a87fc673cf9
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-01 11:57:37 -0400

    compara_arrays method was defined in promedio2.rb

--- promedio2.rb
+++ promedio2.rb
@@ -1,4 +1,4 @@
-#Métodos
+#Método promedio
 def promedio(arr)
     suma = 0
     arr.each do |i|
@@ -6,3 +6,11 @@ def promedio(arr)
     end
     return suma/(arr.count)
 end
+
+#Método compara_arrays
+def compara_arrays(arr1, arr2)
+    promedio_arr1 = promedio(arr1)
+    promedio_arr2 = promedio(arr2)
+    promedio_arr1 > promedio_2 ? return promedio_arr1 : (promedio_arr2 > promedio_arr1 ? return promedio_arr2 : return "Son iguales")
+end
+    
